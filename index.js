0758c1a9ffa8c0ab56c57182fd6ceb8c47556194
danilqweasd12321 <37021861+danilqweasd12321@users.noreply.github.com>
2019-02-09 09:34:02 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var httpProxy = require('http-proxy');
+var express = require('express');
+
+// create server
+var app = express();
+var proxy = httpProxy.createProxyServer({ ws: true });
+var server = require('http').createServer(app);
+
+// proxy HTTP
+app.get('/owop', function(req, res) {
+  try{
+    proxy.web(req, res, {target: "http://52.53.196.35:21"});
+  } catch(e) {
+    
+  }
+});
+
+// Proxy websocket
+server.on('upgrade', function (req, socket, head) {
+  proxy.ws(req, socket, head, {target: "ws://52.53.196.35:21"});
+});
+
+// serve static content
+app.use('/', express.static(__dirname + "/public"));
+
+server.listen(80);
